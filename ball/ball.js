0c3195b25a2dd9d5f3dce3d515b159f6e81292d7
Jukka Ylitalo <juylitalo@gmail.com>
2017-02-09 21:29:27 +0200

    removing unused code

--- ball/ball.js
+++ ball/ball.js
@@ -48,9 +48,6 @@ function init() {
     scene.add(line);
 
     // Materials
-
-    var texture = new THREE.Texture(generateTexture());
-    texture.needsUpdate = true;
  
     materials.push(new THREE.MeshLambertMaterial({ color: 0xdddddd, shading: THREE.SmoothShading }));
 
@@ -59,24 +56,12 @@ function init() {
 
     var geometry = new THREE.SphereGeometry(75, 22, 16);
 
-    for (var i = 0, l = geometry.faces.length; i < l; i++) {
-
-        var face = geometry.faces[i];
-        face.materialIndex = Math.floor(Math.random() * materials.length);
-
-    }
-
-    geometry.sortFacesByMaterialIndex();
-
     objects = [];
 
     //for ( var i = 0, l = materials.length; i < l; i ++ ) {
 
     addMesh(geometry, materials[0]);
 
-    //}
-
-    //addMesh( geometry, new THREE.MultiMaterial( materials ) );
 
     particleLight = new THREE.Mesh(new THREE.SphereGeometry(4, 8, 8), new THREE.MeshBasicMaterial({ color: 0xffffff }));
     scene.add(particleLight);
@@ -123,13 +108,9 @@ function addMesh(geometry, material) {
 
     var mesh = new THREE.Mesh(geometry, material);
 
-    mesh.position.x = 0;// ( objects.length % 4 ) * 200 - 400;
-    mesh.position.z = 0;//Math.floor( objects.length / 4 ) * 200 - 200;
-
-    /*				mesh.rotation.x = Math.random() * 200 - 100;
-                    mesh.rotation.y = Math.random() * 200 - 100;
-                    mesh.rotation.z = Math.random() * 200 - 100;
-    */
+    mesh.position.x = 0; 
+    mesh.position.z = 0; 
+ 
     objects.push(mesh);
 
     scene.add(mesh);
@@ -144,36 +125,7 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 
 }
-
-function generateTexture() {
-
-    var canvas = document.createElement('canvas');
-    canvas.width = 256;
-    canvas.height = 256;
-
-    var context = canvas.getContext('2d');
-    var image = context.getImageData(0, 0, 256, 256);
-
-    var x = 0, y = 0;
-
-    for (var i = 0, j = 0, l = image.data.length; i < l; i += 4, j++) {
-
-        x = j % 256;
-        y = x == 0 ? y + 1 : y;
-
-        image.data[i] = 255;
-        image.data[i + 1] = 255;
-        image.data[i + 2] = 255;
-        image.data[i + 3] = Math.floor(x ^ y);
-
-    }
-
-    context.putImageData(image, 0, 0);
-
-    return canvas;
-
-}
-
+ 
 //
 
 function animate() {
@@ -189,31 +141,11 @@ function render() {
 
     var timer = 0.0001 * Date.now();
 
-    //camera.position.x = Math.cos(timer * 3) * 1000;
-    //camera.position.z = Math.sin(timer * 3) * 1000;
-
     camera.lookAt(scene.position);
 
-    /*   for (var i = 0, l = objects.length; i < l; i++) {
-   
-           var object = objects[i];
-   
-           object.rotation.x += 0.01;
-           object.rotation.y += 0.005;
-   
-       } */
+    line.rotation.z += ( mouseX * 0.0005 - line.rotation.z ) * 0.02;
+    line.rotation.x += ( mouseY * 0.0005 - line.rotation.x ) * 0.02;
 
-    //materials[materials.length - 2].emissive.setHSL(0.54, 1, 0.35 * (0.5 + 0.5 * Math.sin(35 * timer)));
-    //materials[materials.length - 3].emissive.setHSL(0.04, 1, 0.35 * (0.5 + 0.5 * Math.cos(35 * timer)));
-
-    line.rotation.z += ( mouseX * 0.001 - line.rotation.z ) * 0.02;
-    line.rotation.x += ( mouseY * 0.001 - line.rotation.x ) * 0.02;
-	//camera.position.x += ( mouseX - camera.position.x ) * 0.05;
-	//camera.position.y += ( - mouseY - camera.position.y ) * 0.05;
-    //particleLight.position.x = Math.sin(timer * 7) * 300;
-    // particleLight.position.y = Math.cos(timer * 5) * 400;
-    //particleLight.position.z = Math.cos(timer * 3) * 300;
-    
     renderer.render(scene, camera);
 
 }
